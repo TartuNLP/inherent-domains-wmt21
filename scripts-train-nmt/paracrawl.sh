bdd0b8a6463b9cc67bf4c7f49b60eb5474d88992
Maksym Del <max.del.edu@gmail.com>
2021-11-26 18:37:14 +0200

    Revert "Add code"

--- scripts-train-nmt/paracrawl.sh
+++
@@ -1,5 +0,0 @@
-#cd experiments/en-et_paracrawl
-#wget https://s3.amazonaws.com/web-language-models/paracrawl/release7.1/en-et.tmx.gz
-#gunzip en-et.tmx.gz
-#cd ../..
-python scripts-train-nmt/parse_paracrawl_tmx.py
